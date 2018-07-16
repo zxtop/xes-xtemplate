3e0657708eed298ed672b60dd1a1f70b05eb159f
wangqi0902vip <wangqi0902@vip.qq.com>
2018-07-16 20:33:58 +0800

    add console log

--- build/init.js
+++ build/init.js
@@ -6,9 +6,11 @@ const config = require('../config/resource.conf.json')
 const tPath = path.resolve(config.path)
 
 function downloadUrl (urlList) {
+  console.log('Start download resources!')
   urlList.map(url => {
     if(!config.exclude.includes(url.ext)){
       let temp = url.host + url.src.slice(1)
+      console.log('resource:',url.name+'.'+url.ext)
       request(temp).pipe(fs.createWriteStream(tPath+'/'+url.name+'.'+url.ext))
     }
   })
