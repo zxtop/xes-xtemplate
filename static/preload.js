95d2174525bd416eba1c4be474a62ec1973047cb
1256326849@qq.com <81854652251ssy>
2018-07-19 15:09:41 +0800

    样式调整

--- static/preload.js
+++ static/preload.js
@@ -1,46 +1,34 @@
-(function (doc, win) {
-  var docEl = doc.documentElement,
-    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-    recalc = function () {
-      window.clientWidth = docEl.clientWidth;
-      window.clientHeight = docEl.clientHeight;
-      if (!window.clientWidth) return;
-      var aspectRatio = window.clientWidth / window.clientHeight;
-      if (aspectRatio > 1920 / 1080) {
-        docEl.style.fontSize = 100 * (window.clientHeight / 1080) + 'px';
-        window.base = 100 * (window.clientHeight / 1080);
-      } else {
-        docEl.style.fontSize = 100 * (window.clientWidth / 1920) + 'px';
-        window.base = 100 * (window.clientWidth / 1920);
-        // 判断是否为移动设备，提示旋转屏幕
-      }
-      // alert(navigator.userAgent)
-      var isMobile = {
-        Android: function () {
-          return navigator.userAgent.match(/Android/i) ? true : false;
-        },
-        BlackBerry: function () {
-          return navigator.userAgent.match(/BlackBerry/i) ? true : false;
-        },
-        iOS: function () {
-          return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
-        },
-        Windows: function () {
-          return navigator.userAgent.match(/IEMobile/i) ? true : false;
-        },
-        any: function () {
-          return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
-        }
-      };
-    };
+export const pageSizeFun = function (doc, win,widthNum,heightNum) {
+  console.log(widthNum,heightNum);
+  var docEl = doc.documentElement;
+  var resizeEvt = 'orientationchange' in win ? 'orientationchange' : 'resize';
+  var onResize = function () {
+    var clientWidth = docEl.clientWidth;
+    var clientHeight = docEl.clientHeight;
+    var base;
+
+    if (!clientWidth) return;
+
+    var aspectRatio = clientWidth / clientHeight;
+    if (aspectRatio > widthNum / heightNum) {
+      docEl.style.fontSize = 100 * (clientHeight / heightNum) + 'px';
+      base = 100 * (clientHeight / heightNum);
+      win.appLeft = ((clientWidth - widthNum/100 * base) >> 1) + 'px';
+      win.appTop = '0px';
+    } else {
+      docEl.style.fontSize = 100 * (clientWidth / widthNum) + 'px';
+      base = 100 * (clientWidth / widthNum);
+      win.appLeft = '0px';
+      win.appTop = ((clientHeight - heightNum/100 * base) >> 1) + 'px';
+    }
+  };
   try {
-    recalc();
+    onResize();
   } catch (e) {
   }
-     if (!doc.addEventListener) return;
-      win.addEventListener(resizeEvt, recalc, false);
-      doc.addEventListener('DOMContentLoaded', recalc, false);
 
-      document.onreadystatechange = function() { //文档加载完毕
-      }
-  })(document, window);
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, onResize, false);
+  doc.addEventListener('DOMContentLoaded', onResize, false);
+
+};
