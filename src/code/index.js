15914cc664919ded42b342a171cc6c5935c49d76
1256326849@qq.com <81854652251ssy>
2018-07-20 15:38:39 +0800

    事件冒泡处理

--- src/code/index.js
+++ src/code/index.js
@@ -1,4 +1,14 @@
 export function mainFun () {
-  console.log(this)
-  console.log(1123)
+console.log(this)
+
+  this.bgImg.on("xClick",(ev)=>{
+    console.log(ev);
+  });
+
+
+  this.bgImg.on("xDbclick",(ev)=>{
+    console.log(ev);
+  });
+
+
 }
