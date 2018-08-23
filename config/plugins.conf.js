de85b324ddc5a5ae91fe4a20b8cc0823a262eff8
wangqi15 <wangqi15@100tal.com>
2018-08-23 16:33:03 +0800

    add submit

--- config/plugins.conf.js
+++ config/plugins.conf.js
@@ -1,5 +1,5 @@
 module.exports = {
   plugins:['pixi-spine','matter-js','http-server'],
   innerPlugins:['xes-choice','xes_fillvacancy','xes-classification','xes-ligature'],
-  dependencePlugins:['xes-pixi-audio']
+  dependencePlugins:['xes-pixi-audio','xes-submit']
 }
